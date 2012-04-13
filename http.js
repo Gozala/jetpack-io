0809bf53bfabdfb31769775f96fc9032a405a542
Giorgi Kandelaki <kandelakig@gmail.com>
2012-04-13 23:12:12 +0400

    Started working on Node's http.js implementation for jetpack-io

---
+++ http.js
@@ -0,0 +1,23 @@
+const net = require("./net");
+
+function connectionListener(socket) {
+  throw Error('Not yet implemented');
+}
+exports._connectionListener = connectionListener;
+
+const Server = net.Server.extend({
+  constructor: function Server(requestListener) {
+    if (!(this instanceof Server)) return new Server(requestListener);
+  }
+  net.Server.call(this, {}, connectionListener);
+
+  if (requestListener) {
+    this.addListener('request', requestListener);
+  }
+});
+exports.Server = Server;
+
+function createServer(requestListener) {
+  return new Server(requestListener);
+};
+exports.createServer = createServer;
